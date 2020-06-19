bc2b7dce6f44031f649c4aeced1b6537a9d10ea2
Aswathy <aswathyretnakaran@yahoo.com>
2020-06-19 21:17:30 +0530

    Adding sign in changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "../src/pages/home/homepage.component";
 import ShopPage from "../src/components/shop/shop.component";
 import Header from "../src/components/header/header.component";
+import SignInAndSignUpPage from "../src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
